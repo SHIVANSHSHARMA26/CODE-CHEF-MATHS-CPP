9624d30a19ec0186fda51d45e5d68f4b7e4eb5e6
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-03-27 21:17:22 +0530

    Create Chess Time.cpp

---
+++ Chess Time.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,N,N1;
+    cin>>t;
+    while(t--){
+        cin>>N;
+        N1=(N*60)/20;
+        cout<<N1<<"\n";
+    }
+	return 0;
+}
