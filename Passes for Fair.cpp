e3aa0cd2aba5c0b2e9f075fec572dfed837fbf07
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-03-29 23:14:47 +0530

    Create Passes for Fair.cpp

---
+++ Passes for Fair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,N,K;
+	cin>>t;
+	while(t--){
+	    cin>>N>>K;
+	    if(K>N){cout<<"YES"<<"\n";}
+	    else{cout<<"NO"<<"\n";}
+	}
+	return 0;
+}
