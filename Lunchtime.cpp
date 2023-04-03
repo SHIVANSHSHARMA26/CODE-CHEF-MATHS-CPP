6b6c7902559f4aa11b78c0698bef7560ea36d00b
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-03 21:23:11 +0530

    Create Lunchtime.cpp

---
+++ Lunchtime.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,t1;
+	cin>>t;
+	while(t--){
+	    cin>>t1;
+	    if(t1>=1 && t1<=4){cout<<"YES"<<"\n";}
+	    else cout<<"NO"<<"\n";
+	}
+	return 0;
+}
