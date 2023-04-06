8bab0ce10d0204528ca42651885daf4a1ee98ddf
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-06 19:13:56 +0530

    Create Apples and Oranges.cpp

---
+++ Apples and Oranges.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,A,B,sum;
+	cin>>x;
+	cin>>A>>B;
+	sum= A+B;
+	if(x>=sum){cout<<"YES"<<"\n";
+	}
+	else{cout<<"NO"<<"\n";}
+	return 0;
+}
