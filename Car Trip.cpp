2a64a659357e329993210f83fd43e67e7b2a44e6
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-13 21:21:14 +0530

    Create Car Trip.cpp

---
+++ Car Trip.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x>300){cout<<x*10<<"\n";}
+	    else{cout<<3000<<"\n";}
+	}
+	return 0;
+}
