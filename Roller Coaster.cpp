349bd109f52738e06f8e3c70ffeb2ae363273f11
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-17 23:11:40 +0530

    Create Roller Coaster.cpp

---
+++ Roller Coaster.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,h;
+	cin>>t;
+	while(t--){
+	    cin>>x>>h;
+	    if(x>=h){cout<<"YES"<<"\n";}
+	    else{cout<<"NO"<<"\n";}
+	}
+	return 0;
+}
