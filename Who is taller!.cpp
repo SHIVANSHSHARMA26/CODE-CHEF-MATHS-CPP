d2a349839b567d7a467b40ccd5a21643f12e455c
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-17 22:58:20 +0530

    Create Who is taller!.cpp

---
+++ Who is taller!.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b;
+	cin>>t;
+	while(t--){
+	    cin>>a>>b;
+	    if(a>b){
+	       cout<<"A"<<"\n";
+	    }
+	    else{cout<<"B"<<"\n";}
+	}
+	return 0;
+}
