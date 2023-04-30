c7e78f07ea5a73a2808f15d6f624a017a0c71838
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-30 22:07:58 +0530

    Create Perfect Trio.cpp

---
+++ Perfect Trio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+	    cin>>a>>b>>c;
+	    if(a+b==c){cout<<"YES"<<"\n";}
+	    else if(b+c==a){cout<<"YES"<<"\n";}
+	    else if(a+c==b){cout<<"YES"<<"\n";}
+	    else{cout<<"NO"<<"\n";}
+	}
+	return 0;
+}
