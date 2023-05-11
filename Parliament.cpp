db3c6b412deb8c479bcb15c7ec0dddfd7887a3f7
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-05-12 00:49:42 +0530

    Create Parliament.cpp

---
+++ Parliament.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if((x/2)<=y){cout<<"YES"<<"\n";}
+	    else{cout<<"NO"<<"\n";}
+	}
+	return 0;
+}
