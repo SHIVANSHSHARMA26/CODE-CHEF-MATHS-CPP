3699a5c7c4c16227b8c79c3dcc657734509cc37a
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-05-13 21:21:53 +0530

    Create Expert Setter.cpp

---
+++ Expert Setter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	if(y>=(x*0.5)){cout<<"YES"<<"\n";}
+	else{cout<<"NO"<<"\n";}
+	}
+	
+	return 0;
+}
